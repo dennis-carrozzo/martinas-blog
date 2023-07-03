cce7ab4ab403ee8a8c32001cabe769e3af31fdb9
denniswebdel <denniswebdel@gmail.com>
2023-07-03 20:00:18 +0100

    feat (config): Add page component for dynamic routing
    - Created a new component '[...slug].js' to render a page with a title and a StoryblokComponent.
    - Added metadata for SEO and social media sharing.
    - Implemented the functionality to fetch data from the Storyblok API and return it as props for a Next.js page.
    - Designed and implemented the structure of an HTML document in '_document.js'.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,64 @@
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { storyblokInit, apiPlugin } from '@storyblok/react'
+import { ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import AllArticles from '@/components/AllArticles'
+import AllPoems from '@/components/AllPoems'
+import Article from '@/components/Article'
+import Config from '@/components/Config'
+import ContentSection from '@/components/ContentSection'
+import ContentSectionWithCardsGrid from '@/components/ContentSectionWithCardsGrid'
+import FeaturedArticles from '@/components/FeaturedArticles'
+import Grid from '@/components/Grid'
+import GridItem from '@/components/GridItem'
+import Hero from '@/components/Hero'
+import Layout from '@/components/Layout'
+import MenuLink from '@/components/MenuLink'
+import NewsLetterFormSection from '@/components/NewsLetterFormSection'
+import Page from '@/components/Page'
+import Poem from '@/components/Poem'
+import Stack from '@/components/Stack'
+import Typography from '@/components/Typography'
+import theme from '@/theme'
+
+const components = {
+  AllArticles,
+  AllPoems,
+  Article,
+  Config,
+  ContentSection,
+  ContentSectionWithCardsGrid,
+  FeaturedArticles,
+  Grid,
+  GridItem,
+  Hero,
+  MenuLink,
+  NewsLetterFormSection,
+  Page,
+  Poem,
+  Stack,
+  Typography
 }
+
+// initialize the storyblok SDK
+storyblokInit({
+  accessToken: process.env.storyblokPreviewToken,
+  use: [apiPlugin],
+  components
+})
+
+/**
+ * The function `MyApp` is a React component that wraps the provided `Component` with a theme provider,
+ * CSS baseline, and a layout component.
+ */
+function MyApp ({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Layout story={pageProps.config}>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
+  )
+}
+
+export default MyApp
